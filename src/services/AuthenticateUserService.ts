aa433ac221e84a1504c18438dda8f4928b14c6c6
thiago0x01 <thcosta0@hotmail.com>
2021-06-24 17:50:31 -0300

    Aula 04

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,47 @@
+import { getCustomRepository } from "typeorm"
+
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+
+import { UsersRepositories } from "../repositories/UserRepositories"
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({email, password}: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories)
+    
+    // Verificar se email existe
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if(!user) {
+      throw new Error("Email/Password incorrect")
+    }
+    
+    // Verificar se senha está correta
+    const passwordMatch = await compare(password, user.password)
+
+    if(!user) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    // Gerar token
+    const token = sign({
+      email: user.email,
+    }, "107811f1ffc7ee3b816b136c71f54ecb" , {
+      subject: user.id,
+      expiresIn: "1d",
+    })
+
+    return token;
+  }
+
+}
+
+export { AuthenticateUserService }
